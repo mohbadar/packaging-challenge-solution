9478b1225dfee1b89c80b1b4e5cc610a3b58fd16
Mohammad Badar Hashimi <32482095+mohbadar@users.noreply.github.com>
2022-02-01 16:26:12 +0430

    #6 branch and bound algorithm test with 1000 random records

---
+++ src/test/java/eu/unite/challenge/algorithms/BranchAndBoundTest.java
@@ -0,0 +1,39 @@
+package eu.unite.challenge.algorithms;
+
+
+import eu.unite.challenge.dataobjects.Package;
+import eu.unite.challenge.dataobjects.RecordInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BranchAndBoundTest {
+    private static final Logger logger = LoggerFactory.getLogger(BranchAndBoundTest.class);
+
+    @ParameterizedTest
+    @MethodSource("eu.unite.challenge.sources.TestCaseSources#randomSource")
+    void randomCase(final int lineNo, final String line) {
+        logger.trace("{} --- {}\n", lineNo, line);
+
+        RecordInstance p = assertDoesNotThrow(() -> new RecordInstance(lineNo, line));
+        BruteForce bf = new BruteForce(p);
+        BranchAndBound bb = new BranchAndBound(p);
+
+        Package bfBag = bf.getBag();
+        Package bbBag = bb.getBag();
+
+        logger.trace("Brute force result:      " + bfBag);
+        logger.trace("Branch & bound result:   " + bbBag);
+
+        BigDecimal bfPrice = bfBag.getResultPrice();
+        BigDecimal bbPrice = bbBag.getResultPrice();
+
+        assertEquals(bfPrice, bbPrice);
+    }
+}
