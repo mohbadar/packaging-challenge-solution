77b71e8e9d0ae4dcf75a8cc46280bbc805bc5a92
Mohammad Badar Hashimi <32482095+mohbadar@users.noreply.github.com>
2022-02-01 14:18:28 +0430

    #4 item comparator added

---
+++ src/main/java/eu/unite/challenge/algorithms/ItemComparators.java
@@ -0,0 +1,27 @@
+package eu.unite.challenge.algorithms;
+
+
+import eu.unite.challenge.dataobjects.Item;
+
+import java.util.Comparator;
+
+/**
+ * Keeps comparators used by various algorithms.
+ * For instance, {@code priceWeight} is a comparator which
+ * first compares two items using their price,
+ * and if the prices are equal compares their weight.
+ */
+public final class ItemComparators {
+
+    private ItemComparators() {
+    }
+
+    // If their price is equal, the lighter one wins
+    public static final Comparator<Item> priceWeight =
+            Comparator.comparing(Item::getPrice).thenComparing(Item::getWeight, Comparator.reverseOrder());
+
+    // If their efficiency is equal, the expensive one wins
+    public static final Comparator<Item> efficiencyPrice =
+            Comparator.comparing(Item::getEfficiency).thenComparing(Item::getPrice);
+
+}
